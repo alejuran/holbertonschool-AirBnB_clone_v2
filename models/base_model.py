a1189eb4ab8828acede7230032a2a49ae620dbb3
alejuran <alejandrourn@gmail.com>
2022-09-06 15:51:59 -0500

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,8 @@
 import uuid
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime, ForeignKey, Integer
-from sqlalquemy.ext.declarative import declarative_base
-
+from sqlalchemy.ext.declarative import declarative_base
+from models import storage
 
 Base = declarative_base()
 class BaseModel:
@@ -19,24 +19,13 @@ class BaseModel:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.updated_at = datetime.now()  
         else:
-            if kwargs.get('id') is None:
-                kwargs['id'] = str(uuid.uuid4())
-            if kwargs.get('updated_at'):
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                kwargs['updated_at'] = datetime.now()
-            if kwargs.get('created_at'):
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                kwargs['created_at'] = datetime.now()
-            if kwargs.get('__class__'):
-                del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -57,4 +46,6 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary:
+            del dictionary['__sa_instance_state']
         return dictionary
