3798fdd89a36ef662ce27cfb6d095e07bcf946ae
alejuran <alejandrourn@gmail.com>
2022-09-06 17:00:46 -0500

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -6,20 +6,24 @@ from sqlalchemy import Column, String, DateTime, ForeignKey, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from models import storage
 
+
 Base = declarative_base()
+
+
 class BaseModel:
     """A base class for all hbnb models"""
 
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()  
+            self.updated_at = datetime.now()
         else:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
