fd40ae8e6e296a1a0ec8e0d1cdc0e0476af45880
alejuran <alejandrourn@gmail.com>
2022-09-07 10:52:48 -0500

    update place model

--- models/place.py
+++ models/place.py
@@ -8,6 +8,15 @@ from os import getenv
 
 
 HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id', onupdate='CASCADE',
+                                        ondelete='CASCADE'),
+                             primary_key=True),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id', onupdate='CASCADE',
+                                        ondelete='CASCADE'),
+                             primary_key=True))
 
 
 class Place(BaseModel, Base):
