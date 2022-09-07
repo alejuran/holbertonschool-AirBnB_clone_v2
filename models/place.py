3f7a2e517577a895e64647077b3b8bd319dc6795
Santiago23z <santizapataalvares@gmail.com>
2022-09-07 11:17:03 -0500

    place updateS

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 import models
 from os import getenv
