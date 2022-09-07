ee42a499fdd9563ad0da96738adf8aec405ad54a
Santiago23z <santizapataalvares@gmail.com>
2022-09-07 11:43:14 -0500

    updates

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from email.mime import base
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
 
-
 HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """..."""
+    __tablename__ = 'amenities'
     if HBNB_TYPE_STORAGE == "db":
-        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
     else:
         name = ""
