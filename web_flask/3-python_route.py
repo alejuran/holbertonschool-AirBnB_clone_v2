69930b1272bf1941d18c940fcb506515ad4586e3
alejuran <alejandrourn@gmail.com>
2022-09-13 12:22:37 -0500

    python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,7 +27,7 @@ def cisfun(text):
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/(<text>)', strict_slashes=False)
-    def pythoniscool(text='is cool'):
+def pythoniscool(text='is cool'):
     """ display “python ” followed by the value"""
     return 'python ' + text.replace('_', ' ')
 
