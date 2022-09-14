3204a8a23617e2e0f059c6a44bc0849154a37643
alejuran <alejandrourn@gmail.com>
2022-09-13 19:41:46 -0500

    cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Before using Flask to display our HBNB data,
+you will need to update some part of our engine:
+"""
+
+from flask import Flask, render_template
+from models import *
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities():
+    """
+    display a HTML page: (inside the tag BODY)
+    """
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.py', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    closes the storage on teardown
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
